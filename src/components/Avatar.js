4be85d42bb5745efa4df34486b922fc4ed4bd13d
TheSeems <me@theseems.ru>
2021-10-31 14:37:14 +0300

    Change color of the sun (on light mode toggler), cleanup README.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,7 +1,7 @@
 function Avatar() {
     return (
         <img className={"object-cover h-32 filter transform transition duration-700 dark:invert"}
-             src={"img/am.png"}
+             src={"v2/img/am.png"}
              alt={"My avatar"}/>
     )
 }
